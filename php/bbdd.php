1ddc73a3c6b6f197b41f9bb30f1aac22c653db90
leila <leeigregorio@gmail.com>
2022-06-27 09:36:03 -0300

    bbdd

---
+++ php/bbdd.php
@@ -0,0 +1,12 @@
+<?php
+//recibo los datos del formulario
+$nombre=$_GET['nombre'];
+$email=$_GET['email'];
+$telefono=$_GET['telefono'];
+$mensaje=$_GET['mensaje'];
+
+echo "Los datos ingresados";
+echo"$nombre,$email,$telefono y $mensaje";
+
+
+?>
